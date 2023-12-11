46c3ebe9f0db1326b235b8093bdb7c50cb58f59e
Cythonista <gnfx0514@gmail.com>
2023-12-11 22:09:18 +0900

    :green_heart:テストコード追加

--- tests/domain/test_car.py
+++ tests/domain/test_car.py
@@ -1,9 +1,15 @@
-from src.domain import car
+import pytest
+
+from src.domain.car import Car
 from pytest_mock import MockerFixture
 
 
-class TestCar:
+@pytest.fixture
+def test_car():
+    return Car("Car", 100, 100)
 
-    def test_name(self):
-        pass
 
+def test_getter(test_car):
+    assert test_car.get_name == "Car"
+    assert test_car.get_width == 100
+    assert test_car.get_height == 100

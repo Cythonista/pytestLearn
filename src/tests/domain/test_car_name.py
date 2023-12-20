36fe3f685ca44b62993b63ab5b5bc3de267b44a1
Cythonista <gnfx0514@gmail.com>
2023-12-20 22:52:24 +0900

    :green_heart:テストコードの追加

--- src/tests/domain/test_car_name.py
+++ src/tests/domain/test_car_name.py
@@ -1,13 +1,29 @@
-import pytest
+from pytest import fixture
+from pytest import raises
+from dataclasses import FrozenInstanceError
 
 from src.app.domain.car_name import CarName
 
 
 class TestCarName:
-    @pytest.fixture
+    @fixture
     def test_car_name(self):
-        return CarName("TestCar")
+        return CarName("TestCarName")
 
-    def test_getter(self, test_car_name):
-        expected = CarName("TestCar")
+    def test_get_car_name(self, test_car_name):
+        expected = CarName("TestCarName")
         assert test_car_name.get_car_name == expected.get_car_name
+
+    def test_dataclass_frozen(self, test_car_name):
+        with raises(FrozenInstanceError):
+            test_car_name.car_name = "Test"
+
+    def test_dataclass_slots(self, test_car_name):
+        """
+        dataclass slotsのテスト
+
+        :param test_car_name:
+        :return:
+        """
+        with raises(TypeError):
+            test_car_name.test = "Test"
